05cdb34def70f345653e6948c7319f89c009e488
Nasim Reja <nasimreja67@gmail.com>
2023-02-21 22:36:40 +0600

    binary search problem

--- search-algo/binary.cpp
+++ search-algo/binary.cpp
@@ -83,11 +83,97 @@ int peak(vector<int> &v)
     return s;
 }
 
+// Find pivet in an array
+
+// int get_pivet(vector<int> &v)
+// {
+
+//     int s = 0, e = v.size() - 1, mid = s + (e - s) / 2;
+//     while (e > s)
+//     {
+//         if (v[mid] > v[0])
+//             s = mid + 1;
+//         else
+//             e = mid;
+//         mid = s + (e - s) / 2;
+//     }
+//     return s; // e is also will be the answer
+// }
+
+//  find element in a sorted array
+
+int search(vector<int> &nums, int target)
+{
+
+    // find pivet element
+    int sp = 0, ep = nums.size() - 1, midp = sp + (ep - sp) / 2;
+
+    while (ep > sp)
+    {
+        if (nums[midp] >= nums[0])
+            sp = midp + 1;
+        else
+            ep = midp;
+        midp = sp + (ep - sp) / 2;
+    }
+    int pivot = sp;
+
+    // now find the actual element
+
+    int s = 0, e = nums.size() - 1;
+
+    if (target == nums[pivot])
+        return pivot;
+
+    if (nums[pivot] <= target && target <= nums[nums.size() - 1])
+        s = pivot;
+    else
+        e = pivot - 1;
+
+    int mid = s + (e - s) / 2;
+
+    while (s <= e)
+    {
+        if (nums[mid] == target)
+            return mid;
+        else if (target > nums[mid])
+            s = mid + 1;
+        else
+            e = mid - 1;
+
+        mid = s + (e - s) / 2;
+    }
+    return -1;
+}
+
+// sqrt(x)
+int mySqrt(int x)
+{
+    int s = 0, e = x;
+    long long int mid = s + (e - s) / 2;
+    long long int ans = -1;
+    while (e >= s)
+    {
+        long long int squre = mid * mid;
+        if (squre == x)
+            return mid;
+        else if (squre > x)
+            e = mid - 1;
+        else
+        {
+            ans = mid;
+            s = mid + 1;
+        }
+        mid = s + (e - s) / 2;
+    }
+    return ans;
+}
+
 int main()
 {
-    vector<int> v = {2, 3, 4, 5};
-    int arr[6] = {2, 3, 3, 4, 4, 5};
-    cout << leftOcc(arr, 6, 4) << endl;
-    cout << rightOcc(arr, 6, 4);
+    vector<int> v = {5, 1, 3};
+    int n;
+    cin >> n;
+    cout << mySqrt(n) << endl;
     return 0;
 }
